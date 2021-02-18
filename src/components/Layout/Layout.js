ca805122a03c612003d19484e0f7eb82cb6f9688
Abhipatil01 <patilabhijeet.ap@gmail.com>
2021-02-18 21:41:07 +0530

    Added Toolbar, Sidedrawer functionality

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import Wrapper from '../../hoc/Wrapper';
-import classes from './Layout.css';
+import React, { Component } from "react";
+import Wrapper from "../../hoc/Wrapper";
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => {
-  return (
-    <Wrapper>
-      <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={classes.Content}>{props.children}</main>
-    </Wrapper>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  };
 
-export default layout;
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Wrapper>
+    );
+  }
+}
+
+export default Layout;
